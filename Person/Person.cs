3f146ae8c7e4f0a75a5b8bec845d10b3641c4c55
Григорий Поляков <polyakov.gs@students.dvfu.ru>
2023-03-29 14:31:12 +1000

    Отдельная папка с классом карта

---
+++ Person/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDog.Person
+{
+    internal class Person
+    {
+        public char Icon = '@';
+        public int _PersonX;
+        public int _PersonY;
+        public Person(int personX, int personY)
+        {
+            this._PersonX = personX;
+            this._PersonY = personY;
+        }
+    }
+}
